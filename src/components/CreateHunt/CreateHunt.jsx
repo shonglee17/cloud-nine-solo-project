8cb78ee9c44f0a31e62ad6d3a7d0dea8056ae045
Shong Lee <Shonglee17@gmail.com>
2023-02-07 10:48:34 -0600

    finished forms on create hunt page

--- src/components/CreateHunt/CreateHunt.jsx
+++ src/components/CreateHunt/CreateHunt.jsx
@@ -1,16 +1,66 @@
-
-
+import React, { useState } from 'react';
+import { useDispatch } from "react-redux";
 
 
 function CreateHunt(){
+    const [date, setDate] = useState('');
+    const [location, setLocation] = useState('');
+    const [species, setSpecies] = useState('');
+    const [equipment, setEquipment] = useState('');
+    const [restrictions, setRestrictions] = useState('');
+    const dispatch = useDispatch()
+
+    const addHunt = (event) => {
+        event.preventDefault();
+        
+        let huntInfo = {
+            date : date,
+            location : location,
+            species : species,
+            equipment : equipment,
+            restrictions : restrictions
+        }
+        dispatch({
+            type: 'SAGA/ADD_HUNT',
+            payload: huntInfo
+          });
+    }
+
     return (
         <>
-            <div>Date</div>
-            <div>Location</div>
-            <div>Species</div>
-            <div>Equipment</div>
-            <div>Restrictions</div>
-            <div>check mark icon</div>
+            <form onSubmit={addHunt}>
+                <input
+                type="text"
+                value={date}
+                placeholder = "Date"
+                onChange={(event) => setDate(event.target.value)}
+                />
+                <input
+                type="text"
+                value={location}
+                placeholder = "Location"
+                onChange={(event) => setLocation(event.target.value)}
+                />
+                <input
+                type="text"
+                value={species}
+                placeholder = "Species"
+                onChange={(event) => setSpecies(event.target.value)}
+                />
+                <input
+                type="text"
+                value={equipment}
+                placeholder = "Equipment"
+                onChange={(event) => setEquipment(event.target.value)}
+                />
+                <input
+                type="text"
+                value={restrictions}
+                placeholder = "Restrictions"
+                onChange={(event) => setRestrictions(event.target.value)}
+                />
+            </form>
+            <button onClick={addHunt}>Check Mark</button>
         </>
     )
 }
