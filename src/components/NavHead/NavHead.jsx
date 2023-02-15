101e272e4a1666d45d79014c8a5675d6bf6f5a9b
Shong Lee <Shonglee17@gmail.com>
2023-02-15 15:29:53 -0600

    finished styling header

--- src/components/NavHead/NavHead.jsx
+++ src/components/NavHead/NavHead.jsx
@@ -1,30 +1,41 @@
 import './NavHead.css';
-import { Grid, Avatar } from '@material-ui/core';
-import NavigationOutlinedIcon from '@mui/icons-material/NavigationOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
 function NavHead() {
+    const style = {color: "orange"}
+    const hi = () => {
+        console.log('hi');
+    }
+
+    const bye = () => {
+        console.log('bye');
+    }
   return (
     <div className="navIcons">
-      <Grid container spacing={1} alignItems="center">
-        <Grid item>
-          <Avatar>
-            <PersonOutlineOutlinedIcon />
-          </Avatar>
-        </Grid>
-        <Grid item>
-          <Avatar>
-            <img
-              src="https://play-lh.googleusercontent.com/5cpsDta8hXwWPukv1M6TrRfLrBMJxoQq_dmqrQouAwW7B-5wFC1DewgU0_Kqzujf-w"
-              alt="Crosshair"
-            />
-          </Avatar>
-        </Grid>
-        <Grid item>
-          <Avatar>
-            <NavigationOutlinedIcon />
-          </Avatar>
-        </Grid>
-      </Grid>
+      
+        
+          <div className="personIcon">
+            <a
+              
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-mdb-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i style={style} className="fas fa-user" onClick={bye}></i>
+            </a>
+            
+          </div>
+
+          <img className="crosshair" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyiwAQuUxE9bJ8SkaianA82-9Rsswe01dnQzeIoEzK1Q6IxLZxLZQ-1QETHDX1fUJu9D8&usqp=CAU"/>
+
+          <div className="navigation">
+            <a className="nav-link" href="#">
+              <i style = {style} className="fas fa-location-arrow text-orange" onClick={hi}></i>
+            </a>
+          </div>
+        
+      
     </div>
   );
 }
